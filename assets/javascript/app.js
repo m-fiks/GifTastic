cadbca7b079ff3bd2b951e4da536c1f8e0c0eadf
m-fiks <fiksd005@umn.edu>
2018-03-21 17:25:29 -0500

    rating on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,8 +7,8 @@
 
 // https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=MDOKk07DjXXDlXDATCMCt4HFeKhQGtq4&limit=5
 
-let imageURL = [];
-let searchTerm = 'pokemon';
+let imageURL = "";
+let searchTerm = 'charizard';
 let myURL = 'https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=MDOKk07DjXXDlXDATCMCt4HFeKhQGtq4&limit=5';
 
 $.ajax({
@@ -16,15 +16,16 @@ $.ajax({
     method: 'GET'
     }).then(function(response){
         console.log(response)
-        for (let i =0; i < 10; i++){
+        for (let i =0; i < 5; i++){
             imageURL = response.data[i].images.fixed_height.url;
+            let rating = response.data[i].rating;
             //splice out the s index=4
             let addOn = 'http://';
             let sliced = (imageURL.slice(8,imageURL.length));
             let gifURL = addOn.concat(sliced);
             //console.log(gifURL);
-            $('#target').append(`<img src='${gifURL}'> </img>`)
+            $('#target').append(`<div> <h4> Rating: ${rating} </h4> <img src='${gifURL}'> </img> </div>`)
 
         }
-    });
+});
    
