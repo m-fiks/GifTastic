58987f66e426706953b3e1bcb13e74bd1944667a
m-fiks <fiksd005@umn.edu>
2018-03-21 21:51:57 -0500

    input form creates new button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,69 @@
 'use strict';
 $(document).ready(function(){
-// giphy: 
-// MDOKk07DjXXDlXDATCMCt4HFeKhQGtq4
-// -limit:10
-// -rating: 
-// https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=MDOKk07DjXXDlXDATCMCt4HFeKhQGtq4&limit=5
+
+//variables
 let topics = ['pikachu', 'squirtle', 'bulbasaur', 'mew', 'eevee', 'psyduck', 'mewtwo', 'charizard', 'togepi','meowth'];
 let imageURL = "";
 let button;
 let gifSearch;
 
+//clear loaded up gifs
 function clearGifs(){
     $('#target').empty();
 };
 
 function buttonCreation() {
+    //create buttons for array
     for(let i = 0; i <topics.length; i++){
-    let gifTopic = topics[i];
-   
-    //create buttons
-    button = $("<button>");
-    button.addClass("btn");
-    // Added a data-attribute
-    button.attr("name", gifTopic);
-    //console.log(button.attr("name"))
-    button.text(gifTopic);
-    // Addbutton to the buttons div
-    $("#buttons").append(button);
+        let gifTopic = topics[i];
+        //create buttons
+        button = $("<button>");
+        button.addClass("btn");
+        // Added a data-attribute
+        button.attr("name", gifTopic);
+        //console.log(button.attr("name"))
+        button.text(gifTopic);
+        // Addbutton to the buttons div
+        $("#buttons").append(button);
     };
+};
 
-    $('.btn').click(function () {
+//create button from search bar
+$('#search-button').click(function(event){
+    event.preventDefault();
+    let newButton = $('#form').val();
+    topics.push(newButton);
+    //console.log(topics)
+    $('#form').val("");
+    $('#buttons').empty();
+    buttonCreation(); 
+});
+
+
+$('.btn').click(function () {
     clearGifs();
     gifSearch = ($(this).attr("name"));
-    console.log(gifSearch);
+    //console.log(gifSearch);
     let myURL = 'https://api.giphy.com/v1/gifs/search?q=' + gifSearch + '&api_key=MDOKk07DjXXDlXDATCMCt4HFeKhQGtq4&limit=10';
-    $.ajax({
-        url: myURL,
-        method: 'GET'
-        }).then(function(response){
-            //search for gifs
-            for (let i =0; i < 10; i++){
-                imageURL = response.data[i].images.fixed_height_small.url;
-                let rating = response.data[i].rating;
-                //splice out the s index=4
-                let addOn = 'http://';
-                let sliced = (imageURL.slice(8,imageURL.length));
-                let gifURL = addOn.concat(sliced);
-                //console.log(gifURL);
-                $('#target').append(`<div> <h4> Rating: ${rating} </h4> <img src='${gifURL}'> </img> </div>`)
-            };
-        });
+$.ajax({
+    url: myURL,
+    method: 'GET'
+    }).then(function(response){
+        //search for gifs
+        for (let i =0; i < 10; i++){
+            imageURL = response.data[i].images.fixed_height_small.url;
+            let rating = response.data[i].rating;
+            //splice out the s index=4
+            let addOn = 'http://';
+            let sliced = (imageURL.slice(8,imageURL.length));
+            let gifURL = addOn.concat(sliced);
+            //console.log(gifURL);
+            $('#target').append(`<div> <img src='${gifURL}'> </img> <p> Rating: ${rating} </p> </div>`)
+        };
     });
-};
+});
+
+//show initial array buttons
 buttonCreation();
 
 });
