4016a3a0f951baeefabb1f0854c31f0199aad330
m-fiks <fiksd005@umn.edu>
2018-03-28 21:52:01 -0500

    adding pages

---
+++ README.md
@@ -0,0 +1,8 @@
+# GifTastic
+## Utilize Giphy API to create a dynamic web page
+
+* On web page, user can use already established buttons or search for a term to add their own button
+* When clicked, buttons will display 10 gifs related to the term inside of the button
+* Gifs are retrieved from Giphy API using AJAX and displayed using jQuery, HTML, and CSS
+* When clicked on, gifs will begin moving -
+if clicked on again gifs will stop moving
---
+++ assets/javascript/app.js
@@ -0,0 +1,113 @@
+'use strict';
+$(document).ready(function(){
+
+//variables
+let topics = ['pikachu', 'squirtle', 'bulbasaur', 'mew', 'eevee', 'psyduck', 'mewtwo', 'charizard', 'togepi','meowth'];
+let imageURL = "";
+let gifURL = "";
+let animateURL= "";
+let staticURL ="";
+let button;
+let gifSearch;
+let rating;
+
+//clear loaded up gifs
+function clearGifs(){
+    $('#target').empty();
+};
+
+function buttonCreation() {
+    //create buttons for array
+    for(let i = 0; i <topics.length; i++){
+        let gifTopic = topics[i];
+        //create buttons
+        button = $("<button>");
+        button.addClass("btn2");
+        // Added a data-attribute
+        button.attr("name", gifTopic);
+        //console.log(button.attr("name"))
+        button.text(gifTopic);
+        // Addbutton to the buttons div
+        $("#buttons").append(button);
+        //function to call api and get gifs on page
+    };
+    giphySearch();
+};
+
+//create button from search bar
+$('#search-button').click(function(event){
+    event.preventDefault();
+    let newButton = $('#form').val();
+        if (newButton.length > 0) {
+            //console.log(newButton.length)
+            //push to topics array
+            topics.push(newButton);
+            //console.log(topics)
+            $('#form').val("");
+            $('#buttons').empty();
+            buttonCreation(); 
+        } else if (newButton.length === 0){
+            clearGifs();
+            alert('Please enter a term to search for!')
+        };
+});
+
+function giphySearch () {
+$('.btn2').click(function () {
+    clearGifs();
+    gifSearch = ($(this).attr("name"));
+    //console.log(gifSearch);
+    let myURL = 'https://api.giphy.com/v1/gifs/search?q=' + gifSearch + '&api_key=MDOKk07DjXXDlXDATCMCt4HFeKhQGtq4&limit=10';
+
+    $.ajax({
+    url: myURL,
+    method: 'GET'
+    }).then(function(response){
+        //console.log(response)
+
+        //to animate -- change the attr("src", URL)
+
+        //generate gifs
+        for (let i =0; i < 10; i++){
+            
+            //obtain still URL from response object
+            imageURL = response.data[i].images.fixed_height_small_still.url;
+            //obtain animated URL from response object
+            gifURL =  response.data[i].images.fixed_height_small.url;
+            rating = response.data[i].rating;
+            //to get 's' out of https:// - slice out everything after the 's', add 'http://' to beginning
+            let addOn = 'http://';
+            let sliced = (imageURL.slice(8,imageURL.length));
+            let slicedGif = (gifURL.slice(8, gifURL.length));
+            staticURL = addOn.concat(sliced);
+            animateURL = addOn.concat(slicedGif);
+            //console.log(animateURL);
+
+            //append still gif and rating to DOM
+            $('#target').append(`<div id="giphy"> <img src='${staticURL}' data-still=${staticURL} data-animate=${animateURL} data-state="still"> <p> Rating: ${rating} </p> </div>`)
+            console.log(staticURL);
+            };
+        });
+    });
+};
+
+ //animate gifs
+ $('#target').on('click', 'img', function() {            
+    //console.log($(this).attr('data-state'));
+    let state = $(this).attr('data-state');
+    if (state === 'still') {
+        //console.log('here');
+        //change data-state attribute to animate
+        $(this).attr('src', $(this).attr('data-animate'));
+        $(this).attr('data-state','animate');
+    } else if (state === 'animate'){
+        //change data-state attribute to still
+        $(this).attr('src', $(this).attr('data-still'));
+        $(this).attr('data-state','still');
+    };
+});
+
+//show initial array buttons
+buttonCreation();
+
+});
